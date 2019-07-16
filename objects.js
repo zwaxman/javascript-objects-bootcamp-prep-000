9637064d56d74c99a6a603590201b8e48f95bb0e
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 23:49:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
+var playlist = {
+  artist: 'song'
+}
+
 // it('defines an object called `playlist` containing at least one artist-song pair', () => {
 //     expect(typeof playlist).to.equal('object')
 //     expect(Object.keys(playlist).length).to.be.greaterThan(0)
