10d10a674dfeec55368c1f5c0f0305aa532404d9
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 23:53:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,7 @@ function updatePlaylist(playlist,artistName,songTitle){
 
 function removeFromPlaylist(playlist,artistName){
   delete playlist.artistName
+  return playlist
 }
 
 //   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
